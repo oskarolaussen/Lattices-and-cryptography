1dbb19c303f9971decc723686eb6491d359ef73e
Oskar Olaussen <oskar.olaussen@outlook.com>
2018-08-24 19:28:13 +0300

    Basis now input as a file

--- lll.py
+++ lll.py
@@ -1,5 +1,11 @@
-'''This is an implementation of the LLL lattice reduction algorithm. It takes as input the basis of the lattice and its dimension
-and outputs LLL reduced basis that is more orthogonal. Moreover the first vector of the reduced basis solves the approximate shortest vector of the lattice -problem.'''
+'''This is an implementation of the LLL lattice reduction algorithm. The user inputs the name of the file containing the basis on the command line. For example
+"python ggh.py input.txt". The input file must contain a square matrix with base vectors of the lattice as rows.
+Coordinates must be separated by a space.
+The program then outputs LLL reduced basis that is more orthogonal. Moreover the first vector of the reduced basis solves the approximate shortest vector of the lattice -problem.'''
+
+import numpy as np
+import sys
+
 def gramschmidt(basis, dimension):
 	orthogonal_basis=np.empty([dimension, dimension])
 	orthogonal_basis[0]=basis[0]
@@ -11,7 +17,7 @@ def gramschmidt(basis, dimension):
 		for s in range(n):
 			sum=b[n][s]*orthogonal_basis[s]+sum
 		orthogonal_basis[n]=np.subtract(basis[n],sum)
-	return orthogonal_basis, b;
+	return orthogonal_basis, b
 
 def lll(basis, dimension, gramschmidt):
 	w=np.empty([dimension, dimension])
@@ -29,12 +35,23 @@ def lll(basis, dimension, gramschmidt):
 			basis[[k,k-1]]=basis[[k-1,k]]
 			w,u= gramschmidt(basis, dimension)
 			k=max(k-1,1)
-	return basis;
+	return basis
 
-import numpy as np
-dimension=int(input("Dimension of the lattice: "))
-original_basis=np.empty([dimension, dimension])
-for i in range(dimension):
-	original_basis[i]=[float(x) for x in input("Input base vector of the lattice you wish to reduce: ").split()]
+def input_handler(filename):
+	try:
+		with open(filename, 'r') as infile:
+			n = sum((1 for line in infile))
+			matrix = np.empty([n, n])
+			i = 0
+			infile.seek(0)
+			for line in infile:
+				matrix[i] = [int(x) for x in line.split()]
+				i = i + 1
+	except Exception as e:
+		print(e)
+	return matrix, n
+	
+filename = sys.argv[-1]
+original_basis, dimension = input_handler(filename)
 reduced=lll(original_basis, dimension, gramschmidt)
 print(reduced)
